b589e9b211857651ac83345154532ccb0a3be212
pjou00 <peejou90@gmail.com>
2021-12-11 17:22:47 +0800

    Add connection provider.

---
+++ Fosec/Fosec/Database/ConnectionProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Fosec.Database
+{
+    public class ConnectionProvider
+    {
+        public void initCon()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Fosec.mdf;Integrated Security=True");
+            con.Open();
+        }
+    }
+}
