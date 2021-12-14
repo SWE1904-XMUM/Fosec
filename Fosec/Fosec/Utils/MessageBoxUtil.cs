dfd726595bfa731fc1d3a693f8d97d5179b7356d
pjou00 <peejou90@gmail.com>
2021-12-14 17:44:08 +0800

    add message box util

---
+++ Fosec/Fosec/Utils/MessageBoxUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fosec.Utils
+{
+    public class MessageBoxUtil
+    {
+        public void MessageBox(string message)
+        {
+            HttpContext.Current.Response.Write("<script>alert('" + message + "')</script>");
+        }
+    }
+}
