ff2512550d2d403681f1d39ea4484983d6369d07
pjou00 <peejou90@gmail.com>
2021-12-14 21:42:46 +0800

    update signup & login page (TODO -> debug).

--- Fosec/Fosec/SessionManager/SessionManager.cs
+++ Fosec/Fosec/SessionManager/SessionManager.cs
@@ -7,17 +7,17 @@ namespace Fosec.SessionManager
 {
     public class SessionManager
     {
-        public void SetLogin(bool login)
+        public static void SetLogin(bool login)
         {
             HttpContext.Current.Session["login"] = login;
         }
 
-        public void SetUsername(string uname)
+        public static void SetUsername(string uname)
         {
             HttpContext.Current.Session["uname"] = uname;
         }
 
-        public string GetUsername()
+        public static string GetUsername()
         {
             return HttpContext.Current.Session["uname"].ToString();
         }
