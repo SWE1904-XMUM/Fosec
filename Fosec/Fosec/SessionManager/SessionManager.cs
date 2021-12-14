bc05ee5d331f7087ba1494422911cbab2caf7a0d
pjou00 <peejou90@gmail.com>
2021-12-14 21:18:35 +0800

    write session manager function.

--- Fosec/Fosec/SessionManager/SessionManager.cs
+++ Fosec/Fosec/SessionManager/SessionManager.cs
@@ -7,6 +7,19 @@ namespace Fosec.SessionManager
 {
     public class SessionManager
     {
+        public void SetLogin(bool login)
+        {
+            HttpContext.Current.Session["login"] = login;
+        }
 
+        public void SetUsername(string uname)
+        {
+            HttpContext.Current.Session["uname"] = uname;
+        }
+
+        public string GetUsername()
+        {
+            return HttpContext.Current.Session["uname"].ToString();
+        }
     }
 }
